4fde37d6f8e58bc620767ab326232855b36272b5
Pedro Paulo <pedro.paulo.vianna@hotmail.com>
2025-06-02 08:49:13 -0300

    feat: docker compose with application profiles

--- src/main/java/com/pedro/tarefas/TarefasApplication.java
+++ src/main/java/com/pedro/tarefas/TarefasApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import com.pedro.tarefas.enums.Status;
 import com.pedro.tarefas.model.Tarefa;
@@ -17,6 +18,7 @@ public class TarefasApplication {
 	}
 
 	@Bean
+	@Profile("dev")
 	CommandLineRunner initDatabase(TarefasRepository tarefasRepository) {
 		return args -> {
 			tarefasRepository.deleteAll();
